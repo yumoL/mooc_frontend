c28609deba604079015afc0559ee823af89ffe6e
luoyumo <yumo.luo@hotmail.com>
2021-01-06 22:57:16 +0200

    parent and children for categories

--- admin/src/views/admin/category.vue
+++ admin/src/views/admin/category.vue
@@ -1,54 +1,88 @@
 <template>
   <div>
-    <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        Add
-      </button>
-      &nbsp;
-      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-refresh red2"></i>
-        Refresh
-      </button>
+    <div class="row">
+      <div class="col-md-6">
+        <p>
+          <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit"></i>
+            Add
+          </button>
+          &nbsp;
+          <button v-on:click="all()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-refresh red2"></i>
+            Refresh
+          </button>
 
-    </p>
+        </p>
+        <table id="level1-table" class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>sort</th>
+            <th>operations</th>
 
-    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount=5></pagination>
+          </tr>
+          </thead>
 
-    <table id="simple-table" class="table table-bordered table-hover">
-      <thead>
-      <tr>
-      <th>id</th>
+          <tbody>
+          <tr v-for="category in level1" v-on:click="onClickLevel1(category)"
+              v-bind:class="{'active':category.id===active.id}">
+            <td>{{ category.id }}</td>
+            <td>{{ category.name }}</td>
+            <td>{{ category.sort }}</td>
+            <td>
+              <div class="hidden-sm hidden-xs btn-group">
+                <button v-on:click="edit(category)" class="btn btn-xs btn-info">
+                  <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+                <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
+                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-md-6">
+        <p>
+          <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+            <i class="ace-icon fa fa-edit"></i>
+            Add
+          </button>
+        </p>
+        <table id="level2-table" class="table table-bordered table-hover">
+          <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>sort</th>
+            <th>operations</th>
 
-      <th>parent</th>
+          </tr>
+          </thead>
 
-      <th>name</th>
-
-      <th>sort</th>
-      <th>operations</th>
-
-      </tr>
-      </thead>
-
-      <tbody>
-      <tr v-for="category in categorys">
-        <td>{{category.id}}</td>
-        <td>{{category.parent}}</td>
-        <td>{{category.name}}</td>
-        <td>{{category.sort}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(category)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+          <tbody>
+          <tr v-for="category in level2">
+            <td>{{ category.id }}</td>
+            <td>{{ category.name }}</td>
+            <td>{{ category.sort }}</td>
+            <td>
+              <div class="hidden-sm hidden-xs btn-group">
+                <button v-on:click="edit(category)" class="btn btn-xs btn-info">
+                  <i class="ace-icon fa fa-pencil bigger-120"></i>
+                </button>
+                <button v-on:click="del(category.id)" class="btn btn-xs btn-danger">
+                  <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
     <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -96,18 +130,20 @@
 import Pagination from "../../components/pagination";
 
 export default {
-  components: {Pagination},
+  components: {},
   name: "business-category",
   data: function () {
     return {
       category: {}, //new chapter
       categorys: [],
+      level1: [],
+      level2: [],
+      active: {}
     }
   },
   mounted: function () {
     let _this = this;
-    _this.$refs.pagination.size = 5;
-    _this.list(1);
+    _this.all();
   },
   methods: {
     add() {
@@ -122,27 +158,41 @@ export default {
       $("#form-modal").modal("show");
     },
 
-    list(page) {
+    all() {
       let _this = this;
       Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/category/list', {
-        page: page,
-        size: _this.$refs.pagination.size
-      })
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/category/all')
           .then((response) => {
             Loading.hide();
             let resp = response.data;
-            _this.categorys = resp.content.list;
-            _this.$refs.pagination.render(page, resp.content.total);
+            _this.categorys = resp.content;
+
+            //tree structure
+            _this.level1 = [];
+            for (let i = 0; i < _this.categorys.length; i++) {
+              let c = _this.categorys[i];
+              if (c.parent === '00000000') {
+                _this.level1.push(c);
+                for (let j = 0; j < _this.categorys.length; j++) {
+                  let child = _this.categorys[j];
+                  if (child.parent === c.id) {
+                    if (Tool.isEmpty(c.children)) {
+                      c.children = [];
+                    }
+                    c.children.push(child);
+                  }
+                }
+              }
+            }
           })
     },
 
     save() {
       let _this = this;
       //validation
-      if(1 != 1
-        || !Validator.length(_this.category.name, "name", 1, 50)
-        || !Validator.require(_this.category.sort, "sort")
+      if (1 != 1
+          || !Validator.length(_this.category.name, "name", 1, 50)
+          || !Validator.require(_this.category.sort, "sort")
       ) {
         return;
       }
@@ -156,7 +206,7 @@ export default {
             let resp = response.data
             if (resp.success) {
               $("#form-modal").modal("hide");
-              _this.list(1);
+              _this.all();
               Toast.success("A new item has been successfully saved!")
             } else {
               Toast.warning(resp.message);
@@ -173,12 +223,24 @@ export default {
               Loading.hide();
               let resp = response.data
               if (resp.success) {
-                _this.list(1);
+                _this.all();
                 Toast.success("You deleted an item!")
               }
             })
       })
+    },
+
+    onClickLevel1(category) {
+      let _this = this;
+      _this.active = category;
+      _this.level2 = category.children
     }
   }
 }
 </script>
+
+<style scoped>
+.active td {
+  background-color: #d6e9c6 !important;
+}
+</style>
