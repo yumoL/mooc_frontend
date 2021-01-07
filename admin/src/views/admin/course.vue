e68af6c9380b73150c545307f8226b4ff351bc87
luoyumo <yumo.luo@hotmail.com>
2021-01-07 20:47:23 +0200

    integrae zTree for showing category when editing course

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -68,6 +68,14 @@
           <div class="modal-body">
             <form class="form-horizontal">
               <div class="form-group">
+                <label class="col-sm-2 control-label">
+                  category
+                </label>
+                <div class="col-sm-10">
+                  <ul id="tree" class="ztree"></ul>
+                </div>
+              </div>
+              <div class="form-group">
                 <label>name</label>
                 <div col-sm-10>
                   <input v-model="course.name" class="form-control">
@@ -165,6 +173,7 @@ export default {
   mounted: function () {
     let _this = this;
     _this.$refs.pagination.size = 5;
+    _this.initTree()
     _this.list(1);
   },
   methods: {
@@ -252,6 +261,37 @@ export default {
       let _this = this;
       SessionStorage.set("course", course);
       _this.$router.push("/business/chapter");
+    },
+
+    initTree() {
+      var setting = {
+        check: {
+          enable: true
+        },
+        data: {
+          simpleData: {
+            enable: true
+          }
+        }
+      };
+
+      var zNodes = [
+        {id: 1, pId: 0, name: "can check 1", open: true},
+        {id: 11, pId: 1, name: "can check 1-1", open: true},
+        {id: 111, pId: 11, name: "can check 1-1-1"},
+        {id: 112, pId: 11, name: "can check 1-1-2"},
+        {id: 12, pId: 1, name: "can check 1-2", open: true},
+        {id: 121, pId: 12, name: "can check 1-2-1"},
+        {id: 122, pId: 12, name: "can check 1-2-2"},
+        {id: 2, pId: 0, name: "can check 2", checked: true, open: true},
+        {id: 21, pId: 2, name: "can check 2-1"},
+        {id: 22, pId: 2, name: "can check 2-2", open: true},
+        {id: 221, pId: 22, name: "can check 2-2-1", checked: true},
+        {id: 222, pId: 22, name: "can check 2-2-2"},
+        {id: 23, pId: 2, name: "can check 2-3"}
+      ];
+
+      $.fn.zTree.init($("#tree"), setting, zNodes);
     }
   }
 }
