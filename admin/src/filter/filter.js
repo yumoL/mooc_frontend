a8d803c3dabd26dc52237aee8b94f5e714d87f3a
luoyumo <yumo.luo@hotmail.com>
2021-01-12 18:23:29 +0200

    format fize size

--- admin/src/filter/filter.js
+++ admin/src/filter/filter.js
@@ -4,17 +4,17 @@
  * @param key "C"
  */
 let optionKV = (object, key) => {
-    if (!object || !key) {
-        return "";
-    } else {
-        let result = "";
-        for (let enums in object) {
-            if (key === object[enums]["key"]) {
-                result = object[enums]["value"];
-            }
-        }
-        return result
+  if (!object || !key) {
+    return "";
+  } else {
+    let result = "";
+    for (let enums in object) {
+      if (key === object[enums]["key"]) {
+        result = object[enums]["value"];
+      }
     }
+    return result
+  }
 };
 
 /**
@@ -23,17 +23,17 @@ let optionKV = (object, key) => {
  * @param key "C"
  */
 let optionKVArray = (list, key) => {
-    if (!list || !key) {
-        return "";
-    } else {
-        let result = "";
-        for (let i = 0; i < list.length; i++) {
-            if (key === list[i]["key"]) {
-                result = list[i]["value"];
-            }
-        }
-        return result
+  if (!list || !key) {
+    return "";
+  } else {
+    let result = "";
+    for (let i = 0; i < list.length; i++) {
+      if (key === list[i]["key"]) {
+        result = list[i]["value"];
+      }
     }
+    return result
+  }
 };
 
 /**
@@ -42,29 +42,41 @@ let optionKVArray = (list, key) => {
  * @returns {string} 00:08:20
  */
 let formatSecond = (value) => {
-    value = value || 0;
-    let second = parseInt(value, 10);
-    let minute = 0;
-    let hour = 0;
-    if (second > 60) {
-        minute = Math.floor(second / 60);
-        second = Math.floor(second % 60);
-        if (minute > 60) {
-            hour = Math.floor(minute / 60);
-            minute = Math.floor(minute % 60);
-        }
+  value = value || 0;
+  let second = parseInt(value, 10);
+  let minute = 0;
+  let hour = 0;
+  if (second > 60) {
+    minute = Math.floor(second / 60);
+    second = Math.floor(second % 60);
+    if (minute > 60) {
+      hour = Math.floor(minute / 60);
+      minute = Math.floor(minute % 60);
     }
-    let result = "" + PrefixInteger(second, 2) + "";
-    result = "" + PrefixInteger(minute, 2) + ":" + result;
-    result = "" + PrefixInteger(hour, 2) + ":" + result;
-    return result
+  }
+  let result = "" + PrefixInteger(second, 2) + "";
+  result = "" + PrefixInteger(minute, 2) + ":" + result;
+  result = "" + PrefixInteger(hour, 2) + ":" + result;
+  return result
 }
 
 function PrefixInteger(num, length) {
-    return (Array(length).join('0') + num).slice(-length);
+  return (Array(length).join('0') + num).slice(-length);
+}
+
+let formatFileSize = (value) => {
+  value = value || 0;
+  let result;
+  if (value > 100 * 1024) {
+    result = Math.round(value / 1024 / 1024) + "MB";
+  } else {
+    result = Math.round(value / 1024) + "KB";
+  }
+  return result
 }
 
 export default {
-    optionKV,
-    formatSecond
+  optionKV,
+  formatSecond,
+  formatFileSize
 }
--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -34,7 +34,7 @@
         <td>{{file.path}}</td>
         <td>{{file.name}}</td>
         <td>{{file.suffix}}</td>
-        <td>{{file.size}}</td>
+        <td>{{file.size | formatFileSize}}</td>
         <td>{{FILE_USE | optionKV(file.use)}}</td>
 
       </tr>
